76e321c0a515a4b54a784c6c5e97710ba43f3e80
aliceheneghan <heneghan31@gmail.com>
2022-11-08 09:44:38 +0100

    tested prettier configuration

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -9,16 +9,18 @@ const app = express();
 app.use(express.json());
 
 mongoose
-  .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`)
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+  )
   .then(() => {
-    console.log("Database connected!");
+    console.log('Database connected!');
   })
   .catch((error) => {
     console.log(error.message);
   });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, (req, res) => {
-    console.log(`Server is listening on port ${PORT}!`);
+  console.log(`Server is listening on port ${PORT}!`);
 });
