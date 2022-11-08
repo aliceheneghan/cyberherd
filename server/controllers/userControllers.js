418f2d7e7f37a17e1df360093111ca7522295847
johnxadams <johnxadams93@gmail.com>
2022-11-08 15:23:33 +0100

    userController - deleteUser endpoint added

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -59,3 +59,17 @@ export const updateUserData = async (req, res) => {
     return res.status(400).json({ message: error.message });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    return res.status(200).json({ message: 'User successfully deleted' });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,12 @@
 import express from 'express';
 
 // controllers
-import { findAllUsers, registerUser, updateUserData } from '../controllers/userControllers.js';
+import {
+  findAllUsers,
+  registerUser,
+  updateUserData,
+  deleteUser,
+} from '../controllers/userControllers.js';
 
 const router = express.Router();
 
@@ -10,5 +15,7 @@ router.get('/', findAllUsers);
 router.post('/create', registerUser);
 
 router.patch('/update/:id', updateUserData);
+
+router.delete('/delete/:id', deleteUser);
 
 export default router;
