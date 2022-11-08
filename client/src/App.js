6848e1bd78d5ba58a04914e90782a9cb469088c0
luciapannunzio <luciapannunzio@gmail.com>
2022-11-08 09:49:47 +0100

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+// Components
+
+
+// Styles
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
