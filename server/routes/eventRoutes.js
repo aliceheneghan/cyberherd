0b17d0079055aa9e9a387980fab162166dd51cbf
daanvanderputte <daanvanderputte@protonmail.com>
2022-11-08 15:10:33 +0100

    creates eventRoutes get and post

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -9,9 +9,21 @@ router.get('/', async(req, res) => {
 });
 
 router.post('/create', async(req, res) => {
-    
+    try {
+        const {name, date, time, location, tickets, genre, information} = req.body;
+        const createdEvent = await Event.create({
+            name,
+            date,
+            time,
+            location,
+            tickets,
+            genre,
+            information
+        });
+        return res.status(200).json({message: 'Event created', createdEvent})
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }  
 });
-
-
 
 export default router;
