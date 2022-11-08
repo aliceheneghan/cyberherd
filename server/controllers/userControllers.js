8d966e9f9cfe581ea094d319171831c25c15e79f
johnxadams <johnxadams93@gmail.com>
2022-11-08 15:13:14 +0100

    userController - PATCH updateUserData added

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,4 @@
-import User from "../models/userModel.js";
+import User from '../models/userModel.js';
 import bcrypt from 'bcrypt';
 
 export const findAllUsers = async (req, res) => {
@@ -33,7 +33,28 @@ export const registerUser = async (req, res) => {
     });
     console.log('User successfully registered 👌️');
 
-    return res.status(200).json({ message: 'User successfully created' , createdUser});
+    return res
+      .status(200)
+      .json({ message: 'User successfully created', createdUser });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+export const updateUserData = async (req, res) => {
+  try {
+    const { email, userName, password, photoURL } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const updatedUserData = await User.findByIdAndUpdate(
+      req.params.id,
+      { email, userName, password: hashedPassword, photoURL },
+      { new: true }
+    );
+    return res
+      .status(200)
+      .json({ message: 'User data successfully updated', updatedUserData });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
 // controllers
-import { findAllUsers, registerUser } from '../controllers/userControllers.js';
+import { findAllUsers, registerUser, updateUserData } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
 router.get('/', findAllUsers);
 
 router.post('/create', registerUser);
+
+router.patch('/update/:id', updateUserData);
 
 export default router;
