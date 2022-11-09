b29596438bb70cc7063182a509d23dcffce0c9e6
aliceheneghan <heneghan31@gmail.com>
2022-11-09 11:23:26 +0100

    imported Calendar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 // Components
 import Banner from './components/Banner.jsx';
+import Calendar from "./components/Calendar.jsx";
 
 // Styles
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
 
       <Banner />
+      <Calendar />
       <Routes>
        {/* <Route path="/" element={}/>  */}
       </Routes>
