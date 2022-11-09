30bc394ab60ef39576ceed7c1c845f6809b8f8ee
aliceheneghan <heneghan31@gmail.com>
2022-11-09 22:24:45 +0100

    created span classes to position grid columns

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+// Style
+import "../scss/layout/_calendar.scss"
 
 export default function Calendar() {
   return (
-    <div>
-        <h2>Calendar</h2>
-        <div class="calendar">
-            <div class="month-toggle"></div>
+    <div class="calendar">
+        <div class="month-selector">
+            <div class="span-2">{"<"}</div>
+            <div class="span-3">Month</div>
+            <div class="span-2">{">"}</div>
         </div>
-        
     </div>
   )
 }
