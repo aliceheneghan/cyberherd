ee208fcef7ad0f334b473855eaf5ac50fbd9598f
aliceheneghan <heneghan31@gmail.com>
2022-11-10 20:15:02 +0100

    created Cell component

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 // Style
 import "../scss/layout/_calendar.scss"
+// Components
+import Cell from './Cell';
 
 export default function Calendar() {
   return (
     <div class="calendar">
         <div class="month-selector">
-            <div class="span-2">{"<"}</div>
-            <div class="span-3">Month</div>
-            <div class="span-2">{">"}</div>
+          <Cell> {"<"} </Cell>
+          <Cell>Month</Cell>
+          <Cell>{">"}</Cell>
         </div>
     </div>
   )
---
+++ client/src/components/Cell.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Cell({ children }) {
+  return (
+    <div className="MonthSelector">{children}</div>
+  )
+}
