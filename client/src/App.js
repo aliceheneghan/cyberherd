b512ba5e195e6ae8beda2cf8dd9de86047d67d10
luciapannunzio <luciapannunzio@gmail.com>
2022-11-10 12:06:07 +0100

    Navbar imported

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 // Components
 import Banner from './components/Banner.jsx';
 import Calendar from "./components/Calendar.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 // Styles
 import './App.css';
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
 
       <Banner />
+      <Navbar />
       <Calendar />
       <Routes>
        {/* <Route path="/" element={}/>  */}
