a0a5256a8339f61f9bb6f49be6c300654566c706
johnxadams <johnxadams93@gmail.com>
2022-11-10 10:46:42 +0100

    naming convention issues

--- server/middleware/userValidation.js
+++ server/middleware/userValidation.js
@@ -7,16 +7,16 @@ const validateUser = [
   body('userName')
     .exists()
     .trim()
-    .isLength({ min: 1, max: 20 })
+    .isLength({ min: 4, max: 20 })
     .withMessage(
-      'Firstname should not be empty, and be more than one and less than 20 characters'
+      'Username should be between 4 and 20 characters.'
     ),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res
         .status(400)
-        .json({ message: 'Validation errors userName', error: errors });
+        .json({ message: 'Please choose username.', error: errors });
     }
 
     next();
