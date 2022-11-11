2f5c3deacf2fe7d33f06dcdb263122c20b9de0b6
aliceheneghan <heneghan31@gmail.com>
2022-11-11 20:42:08 +0100

    removed className from CalendarMonth cells

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 // Style
 import "../scss/layout/_calendar.scss"
 // Components
-import Cell from '../features/calendar/components/Cell';
+import Cell from './Cell';
 
 export default function Calendar() {
   return (
     <div className="Calendar">
-        <div class="CalendarMonth">
-          <Cell className=""> {"<"} </Cell>
-          <Cell className="">Month</Cell>
-          <Cell className="">{">"}</Cell>
+        <div className="CalendarMonth">
+          <Cell> {"<"} </Cell>
+          <Cell>Month</Cell>
+          <Cell>{">"}</Cell>
         </div>
-        <div class="CalendarDays SevenDayGrid">
+        <div className="CalendarDays SevenDayGrid">
           <Cell>Mon</Cell>
           <Cell>Tue</Cell>
           <Cell>Wed</Cell>
