a9edf990a6204b68526986adee6035cd8f07623c
aliceheneghan <heneghan31@gmail.com>
2022-11-17 11:52:08 +0100

    imported Cell and created calendar month row with arrows

--- client/src/features/calendar/components/Calendar.jsx
+++ client/src/features/calendar/components/Calendar.jsx
@@ -1,31 +1,27 @@
 // libraries
-import React from 'react';
-import { startOfMonth } from 'date-fns'; 
+import { useState } from 'react';
 
 // style
-import "./_calendar.scss";
+import './_calendar.scss';
 
 // components
 import Cell from './Cell';
 
-// data
-const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 export default function Calendar() {
+  
+
   return (
     <div className="calendar">
-        <div className="calendar-month">
-          <Cell> {"<"} </Cell>
-          <Cell>Month</Cell>
-          <Cell>{">"}</Cell>
-        </div>
-        <div className="calendar-days seven-day-grid">
-          {daysOfWeek.map((day) => (
-            <Cell key={day}>
-              {day}
-            </Cell>
-          ))}
-        </div>
+      <div className="calendar-month">
+        <Cell>
+          {'<'}
+        </Cell>
+        <Cell>Month</Cell>
+        <Cell>
+          {'>'}
+        </Cell>
+      </div>
     </div>
-  )
+  );
 }
