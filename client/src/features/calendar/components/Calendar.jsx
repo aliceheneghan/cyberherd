ed9de8966e088c5dec6fc91b2163a0305ee07a3f
aliceheneghan <heneghan31@gmail.com>
2022-11-17 12:25:44 +0100

    used currentDate and date-fns functions to extract start and end of month and calculate each day of that interval

--- client/src/features/calendar/components/Calendar.jsx
+++ client/src/features/calendar/components/Calendar.jsx
@@ -1,6 +1,6 @@
 // libraries
 import { useState } from 'react';
-import { format } from 'date-fns';
+import { format, startOfMonth, endOfMonth, eachDayOfInterval } from 'date-fns';
 
 // style
 import './_calendar.scss';
@@ -15,6 +15,11 @@ export default function Calendar() {
   // state
   const [currentDate, setCurrentDate] = useState(new Date());
 
+  // date variables (extracted from currentDate)
+  const monthStart = startOfMonth(currentDate);
+  const monthEnd = endOfMonth(currentDate);
+  const monthDays = eachDayOfInterval({ start: monthStart, end: monthEnd });
+
   return (
     <div className="calendar">
       <div className="calendar-month">
@@ -23,9 +28,19 @@ export default function Calendar() {
         <Cell>{format(currentDate, 'LLLL yy')}</Cell>
         <Cell>{'>'}</Cell>
       </div>
+
       <div className="calendar-days seven-day-grid">
         {daysOfWeek.map((day) => (
           <Cell key={day}>{day}</Cell>
+        ))}
+      </div>
+
+      <div className="seven-day-grid">
+        {monthDays.map((date, index) => (
+          <Cell key={index}>
+            {/* formats JS date into day */}
+            {format(date, 'd')}
+          </Cell>
         ))}
       </div>
     </div>
