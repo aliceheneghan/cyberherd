a77b1bcf78a768bdd8472f9b1b41b9c7ac1471d0
johnxadams <johnxadams93@gmail.com>
2022-11-21 10:32:27 +0100

    bug signUp js

--- client/src/features/sign-up-form/components/SignUp.jsx
+++ client/src/features/sign-up-form/components/SignUp.jsx
@@ -7,101 +7,107 @@ import { useNavigate } from 'react-router-dom';
 import './_sign-up.scss';
 
 export default function SignUp() {
- const [userName, setUserName] = useState("");
- const [email, setEmail] = useState("");
- const [password, setPassword] = useState("");
- const [dateOfBirth, setDateOfBirth] = useState("");
- const [image, setImage] = useState("");
- const [error, setError] = useState("");
- 
- const navigate = useNavigate();
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [image, setImage] = useState('');
+  const [error, setError] = useState('');
 
- const handleSubmit = async (e) => {
-  e.preventDefault();
+  const navigate = useNavigate();
 
-  console.log("testing123");
-  const formData = new FormData(e.target);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-// const user =  {
-//   userName: formData.get("userName"),
-//   email: formData.get("email"),
-//   password: formData.get("password"),
-//   dateOfBirth: formData.get("dateOfBirth"),
-//   // image: formData.get("image")
-// }
-// console.log(user)
-  try {
-    await axios.post(`http://localhost:4000/api/user/create`, formData,
-      {
-        headers: { "Content-Type": "multipart/form-data" },
-      }
-    );
-    setError("");
-    navigate("/login");
-  } catch (error) {
-    setError(error)
-    console.log(error)
-  }
- };
+    console.log('testing123');
+    const formData = new FormData(e.target);
 
- return (
-  <div className='connect-page'>
+    // const user =  {
+    //   userName: formData.get("userName"),
+    //   email: formData.get("email"),
+    //   password: formData.get("password"),
+    //   dateOfBirth: formData.get("dateOfBirth"),
+    //   // image: formData.get("image")
+    // }
+    // console.log(user)
+    try {
+      await axios.post(`http://localhost:4000/api/user/create`, formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+      setError('');
+      navigate('/login');
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    }
+  };
 
-{/* <img src="http://localhost:4000/images/32ab7a21e3b9bbbe00fc333276a68951"/> */}
+  return (
+    <div className="connect-page">
+      {/* <img src="http://localhost:4000/images/32ab7a21e3b9bbbe00fc333276a68951"/> */}
 
-    <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
+        <label for="user-name">
+          <input
+            id="user-name"
+            onChange={(e) => setUserName(e.target.value)}
+            type="text"
+            value={userName}
+            placeholder="userName"
+            name="userName"
+          />
+        </label>
 
-    <label for='user-name'>
-    <input id='user-name'
-    onChange={(e) => setUserName(e.target.value)}
-    type='text' 
-    value={userName}
-    placeholder='userName' 
-    name="userName"
-    />
-    </label>
+        <label for="email">
+          <input
+            id="email-sign"
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            value={email}
+            placeholder="email"
+            name="email"
+          />
+        </label>
 
-    <label for='email'>
-    <input id='email-sign'
-    onChange={(e) => setEmail(e.target.value)}
-    type='email' 
-    value={email}
-    placeholder='email' 
-    name="email"
-    />
-    </label>
-    
-    <label for='password'>
-    <input id='password-sign'
-    onChange={(e) => setPassword(e.target.value)}
-    type='password' 
-    value={password}
-    placeholder='password' 
-    name="password"
-    />
-    </label>
-    
-    <label for='dob'>
-    <input id='dob'
-    onChange={(e) => setDateOfBirth(e.target.value)}
-    type='date' 
-    value={dateOfBirth}
-    placeholder='dateOfBirth' 
-    name="dateOfBirth"
-    />
-    </label>
-    
-    <div className="file-upload">
-    <label>Select image for upload </label>
-    <input id='image-upload' type='file' accept='image/jpg' name="image" />
-    </div>
+        <label for="password">
+          <input
+            id="password-sign"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            value={password}
+            placeholder="password"
+            name="password"
+          />
+        </label>
 
-    <br/>
-    <button className='button-sign' type='submit'>Sign Up</button>
-    </form>
+        <label for="dob">
+          <input
+            id="dob"
+            onChange={(e) => setDateOfBirth(e.target.value)}
+            type="date"
+            value={dateOfBirth}
+            placeholder="dateOfBirth"
+            name="dateOfBirth"
+          />
+        </label>
 
-    <div>
-      {error ? <p>{error}</p> : null}
+        <div className="file-upload">
+          <label>Select image for upload </label>
+          <input
+            id="image-upload"
+            type="file"
+            accept="image/jpg"
+            name="image"
+          />
+        </div>
+
+        <br />
+        <button className="button-sign" type="submit">
+          Sign Up
+        </button>
+      </form>
+
+      <div>{error ? <p>{error}</p> : null}</div>
     </div>
   );
 }
