4f41e7f9ddd90d14727eb7abd57255c9381cb482
johnxadams <johnxadams93@gmail.com>
2022-11-22 13:01:13 +0100

    eventPage created and schema API added

--- client/src/features/event-info/components/EventData.jsx
+++ client/src/features/event-info/components/EventData.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+// libraries
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 export default function EventData() {
+  const [eventData, setEventData] = useState({});
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getUser = async (e) => {
+      const { data } = await axios.get(
+        `http://localhost:4000/api/events/${id}`
+      );
+      console.log(`dashboard testing to see:`);
+      console.log(`EventPage data:`, data);
+      setEventData(data);
+    };
+    getUser();
+  }, [id]);
+
   return (
     <section className="event-data-container">
       <div className="event-date-price-container">
-        <div className="event-date">01.12.2022</div>
-        <div className="event-price">15.99 €</div>
+        <div className="event-date">{eventData?.event?.date}</div>
+        <div className="event-price">{eventData?.event?.tickets?.preSalePrice} €</div>
       </div>
       <div className="event-info-img-container">
         <div className="event-info-container">
-          <div className="event-name"> Yoko Suna strikes back - unapologetically</div>
+          <div className="band-event-name-container">
+            <div className="band-name">{eventData?.event?.name?.bandName}</div>
+            <div className="event-name">{eventData?.event?.name?.eventName}</div>
+          </div>
           <div className="event-description-container">
             <div className="event-location-time-container">
-              <div className="event-location">Location</div>
-              <div className="event-time">19:30 Entering</div>
+              <div className="event-location">{eventData?.event?.location}</div>
+              <div className="event-time"> {eventData?.event?.time?.doorsOpen} doors Open</div>
             </div>
             <div className="event-description">
-              <p>Yoko Suna is an outstanding lorem ipsum dolor sit amet consectetur adipisicing elit. Bass iure mollitia reiciendis culpa. Possimus porro ab Berlin, totam aut dicta placeat commodi, tenetur voluptates laboriosam quae adipisci nulla unde voluptas? Kreuzberg ipsum dolor sit amet.</p>
+              <p>{eventData?.event?.information?.description}</p>
             </div>
             <div className="saveDate-buyTicket-container">
               <button className="save-date-btn">Save the Date</button>
@@ -24,7 +46,13 @@ export default function EventData() {
             </div>
           </div>
         </div>
-        <div className="event-img-container"></div>
+        <div className="event-img-container">
+          <img
+            className="event-image"
+            src={eventData?.event?.photoURL}
+            alt=""
+          />
+        </div>
       </div>
     </section>
   );
