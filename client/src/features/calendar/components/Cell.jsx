8e20fd47d8feee65f6143a879b333a623017b8bc
aliceheneghan <heneghan31@gmail.com>
2022-11-22 12:15:50 +0100

    created dateOnClick handler and if statement to determine which version of Cell component to return

--- client/src/features/calendar/components/Cell.jsx
+++ client/src/features/calendar/components/Cell.jsx
@@ -1,13 +1,30 @@
 // libraries
-import React from 'react';
+import { useState} from 'react';
 
 // components
 import UnstyledLink from './UnstyledLink';
 
-export default function Cell({ className, children, handler, cb }) {
-  return (
-    <div className={className} onClick={() => handler(cb)}>
-      <UnstyledLink to='/'>{children}</UnstyledLink>
-    </div>
-  );
+export default function Cell({ className, children, handler, cb, dateObject }) {
+ 
+
+  // handler
+  const dateOnClick = (e) => {
+    console.log(dateObject);
+  };
+
+  if (className === "date-cell") {
+    return (
+      <div className={className}  onClick={dateOnClick}>
+        <UnstyledLink to='/'>{children}</UnstyledLink>
+      </div>
+    );
+
+  } else {
+    return (
+      <div onClick={() => handler(cb)}>
+        {children}
+      </div>
+    );
+  }
+  
 }
