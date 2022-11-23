708a60217b29982508c51043c39a91d5f8dc4e79
aliceheneghan <heneghan31@gmail.com>
2022-11-23 12:50:38 +0100

    passed date to .date-cell Cell component

--- client/src/features/calendar/components/Calendar.jsx
+++ client/src/features/calendar/components/Calendar.jsx
@@ -1,6 +1,13 @@
 // libraries
 import { useState } from 'react';
-import { format, startOfMonth, endOfMonth, eachDayOfInterval, sub, add } from 'date-fns';
+import {
+  format,
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  sub,
+  add,
+} from 'date-fns';
 
 // style
 import './_calendar.scss';
@@ -25,40 +32,43 @@ export default function Calendar() {
   // date variables (extracted from currentDate)
   const monthStart = startOfMonth(currentDate);
   const monthEnd = endOfMonth(currentDate);
-  const monthDays = eachDayOfInterval({ start: monthStart, end: monthEnd });
+  const monthDays = eachDayOfInterval({ start: monthStart, end: monthEnd }); // returns an array of objects
   const prefixWeekdays = monthStart.getDay() - 1; // getDay calculates days of week before the start of the month based on Sunday as the first weekday, so -1 needed to align with Monday week start
 
   return (
     <div className="calendar-wrapper">
       <div className="calendar">
-      <div className="calendar-month">
-        <Cell handler={onClick} cb={prevMonth}>
-          {'<'}
-        </Cell>
-        {/* formats JS date into month and year */}
-        <Cell>{format(currentDate, 'LLLL yy')}</Cell>
-        <Cell handler={onClick} cb={nextMonth}>
-          {'>'}
-        </Cell>
-      </div>
-
-      <div className='seven-day-grid calendar-days'>
-        {daysOfWeek.map((day) => (
-          <Cell key={day} className="weekdays">{day}</Cell>
-        ))}
-      
-        {Array.from({ length: prefixWeekdays }).map((_, index) => (
-          <Cell key={index} className="date-cell"></Cell>
-        ))}
-
-        {monthDays.map((date, index) => (
-          <Cell key={index} className="date-cell" dateObject={date}>
-            {/* formats JS date into day */}
-            {format(date, 'd')}
+        <div className="calendar-month">
+          <Cell handler={onClick} cb={prevMonth}>
+            {'<'}
           </Cell>
-        ))}
+          {/* formats JS date into month and year */}
+          <Cell>{format(currentDate, 'LLLL yy')}</Cell>
+          <Cell handler={onClick} cb={nextMonth}>
+            {'>'}
+          </Cell>
+        </div>
+
+        <div className="seven-day-grid calendar-days">
+          {daysOfWeek.map((day) => (
+            <Cell key={day} className="weekdays">
+              {day}
+            </Cell>
+          ))}
+
+          {Array.from({ length: prefixWeekdays }).map((_, index) => (
+            <Cell key={index} className="date-cell"></Cell>
+          ))}
+
+          {monthDays.map((date, i) => (
+            <Cell key={i} className="date-cell" date={date}>
+              {/* formats JS date into day */}
+
+              {format(date, 'd')}
+            </Cell>
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
