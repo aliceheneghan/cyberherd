33064100d900bb50032d8164cbcdc07ebd59c70d
johnxadams <johnxadams93@gmail.com>
2022-11-23 15:42:23 +0100

    LoginToken inside context added - eventPage.Bookmarking events init

--- client/src/features/event-info/components/EventData.jsx
+++ client/src/features/event-info/components/EventData.jsx
@@ -20,28 +20,48 @@ export default function EventData() {
     getUser();
   }, [id]);
 
+  const handleSaveDate = async (id) => {
+    try {
+      const response =
+        await axios.patch(`http://localhost:4000/api/events/${id}
+      `);
+      console.log(response.data);
+    } catch (error) {}
+  };
+handleSaveDate(eventData?.event?.id)
+
+
   return (
     <section className="event-data-container">
       <div className="event-date-price-container">
         <div className="event-date">{eventData?.event?.date}</div>
-        <div className="event-price">{eventData?.event?.tickets?.preSalePrice} €</div>
+        <div className="event-price">
+          {eventData?.event?.tickets?.preSalePrice} €
+        </div>
       </div>
       <div className="event-info-img-container">
         <div className="event-info-container">
           <div className="band-event-name-container">
             <div className="band-name">{eventData?.event?.name?.bandName}</div>
-            <div className="event-name">{eventData?.event?.name?.eventName}</div>
+            <div className="event-name">
+              {eventData?.event?.name?.eventName}
+            </div>
           </div>
           <div className="event-description-container">
             <div className="event-location-time-container">
               <div className="event-location">{eventData?.event?.location}</div>
-              <div className="event-time"> {eventData?.event?.time?.doorsOpen} doors Open</div>
+              <div className="event-time">
+                {' '}
+                {eventData?.event?.time?.doorsOpen} doors Open
+              </div>
             </div>
             <div className="event-description">
               <p>{eventData?.event?.information?.description}</p>
             </div>
             <div className="saveDate-buyTicket-container">
-              <button className="save-date-btn">Save the Date</button>
+              <button className="save-date-btn" onClick={handleSaveDate}>
+                Save the Date
+              </button>
               <button className="buy-ticket-btn">Buy Ticket</button>
             </div>
           </div>
