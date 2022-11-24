9d66a9dd5c7a36ad3d06bae2ec02ab09e36a4e75
aliceheneghan <heneghan31@gmail.com>
2022-11-24 13:14:44 +0100

    created getEvents function and useEffect where it is called

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -1,8 +1,24 @@
 // libraries
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Results() {
+  const { date } = useParams();
+
+  const getEvents = async () => {
+    const { data } = await axios.get(`http://localhost:4000/api/events`);
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  // console.log(events[0]);
+
   return (
-    <div>Results</div>
-  )
+    <div>
+      <h1>Results</h1>
+    </div>
+  );
 }
