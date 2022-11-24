c96250f065a11f8ae8fed94c243254b8b1a2529a
daanvanderputte <daanvanderputte@protonmail.com>
2022-11-24 16:35:19 +0100

    small changes ResultData

--- client/src/features/results/components/ResultData.jsx
+++ client/src/features/results/components/ResultData.jsx
@@ -3,22 +3,4 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
-export default function ResultData() {
-    const { date } = useParams();
-  
-    const getEvents = async () => {
-      const { data } = await axios.get(`http://localhost:4000/api/events`);
-    };
-  
-    useEffect(() => {
-      getEvents();
-    }, []);
-  
-    // console.log(events[0]);
-  
-    return (
-      <div>
-        <h1>Results</h1>
-      </div>
-    );
-  }
+// export default function 
