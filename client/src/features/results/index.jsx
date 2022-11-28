813130898fbbb4e1037915fc6a6fa27b89bb00d2
luciapannunzio <luciapannunzio@gmail.com>
2022-11-28 10:46:45 +0100

    Banner changed to a single color

--- client/src/features/results/index.jsx
+++ client/src/features/results/index.jsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-// import ResultData from './components/ResultData.jsx';
-
-// export { ResultData };
-=======
 import EventList from './components/EventList';
 
 export { EventList };
->>>>>>> b9a2634d54df16bbdef24aaed33ffca5458ba2a1
