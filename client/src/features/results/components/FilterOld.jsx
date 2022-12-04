0cf917c80e69cb99d3d8f69eab79f34b64e3cded
daanvanderputte <daanvanderputte@protonmail.com>
2022-12-04 13:12:13 +0100

    trying out filter options

---
+++ client/src/features/results/components/FilterOld.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+// filter variables
+
+const genre = [
+  'Acoustic',
+  'Afrobeat',
+  'Alternative',
+  'Electronic',
+  'Experimental',
+  'Folk',
+  'Hardcore',
+  'Hip-Hop',
+  'Indie',
+  'Krautrock',
+  'Metal',
+  'Rock',
+  'Pop',
+  'Post-Punk',
+  'Psychedelic',
+  'Punk',
+  'Rap',
+  'Shoegaze',
+  'Singer Songwriter',
+  'Stoner',
+  'Trap',
+];
+const price = [];
+const neighborhood = [
+  'Charlottenburg',
+  'Friedrichshain',
+  'Hellersdorf',
+  'Köpenick',
+  'Kreuzberg',
+  'Lichtenberg',
+  'Marzahn',
+  'Mitte',
+  'Neukölln',
+  'Pankow',
+  'Prenzlauer Berg',
+  'Reinickendorf',
+  'Schöneberg',
+  'Schöneweide',
+  'Spandau',
+  'Steglitz',
+  'Tempelhof',
+  'Treptow',
+  'Wedding',
+  'Zehlendorf',
+];
+const venueType = ['bar', 'café', 'club', 'concert hall', 'cultural center'];
+
+export default function Filter() {
+  const [resultData, setResultData] = useState([]);
+
+  const { date } = useParams();
+
+  useEffect(() => {
+    const getEvent = async (e) => {
+      const { data } = await axios.get(
+        `http://localhost:4000/api/events/date/${date}`
+      );
+
+      setResultData(data.event);
+    };
+    getEvent();
+  }, [date]);
+
+ 
+ const handleRadioSelect = resultData.filter(result => {
+    return result.genre.toString() === "Rock";
+});
+
+  return (
+    <div>
+      <div className="radio-button">
+        <input
+          type="radio"
+          name="genre"
+          value="Rock"
+          checked={false}
+          onChange={handleRadioSelect}
+        ></input>
+      </div>
+    </div>
+  );
+}
+
+// export default function Filter() {
+
+//   return (
+
+//     <div>
+
+//       <div className='filter-categories'>
+//         <div>genre</div>
+//         <div>price</div>
+//         <div>neighborhood</div>
+//         <div>venue type</div>
+//         </div>
+
+//       <div className="filer-radio-boxes">
+//         <label>
+//           <input type="radio" name="genre" value={genre}></input>Electronic
+//         </label>
+//         <label>
+//           <input type="radio" name="genre" value={genre} checked={}></input>Hip-Hop
+//         </label>
+//         <label>
+//           <input type="radio" name="genre" value={genre}></input>Pop
+//         </label>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// return (
+//     <div>
+//       {venueType
+//         .filter((type) => type === venueType[2])
+//         .map((filteredGenre) => (
+//           <li>{filteredGenre}</li>
+//         ))}
+//     </div>
+//   );
