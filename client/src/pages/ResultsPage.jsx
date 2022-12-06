8e6ebee371e4786475b72202afd7ebb44a04a19b
aliceheneghan <heneghan31@gmail.com>
2022-12-06 12:39:33 +0100

    called Calendar component with different displayType params

--- client/src/pages/ResultsPage.jsx
+++ client/src/pages/ResultsPage.jsx
@@ -5,15 +5,16 @@ import Navbar from '../features/navbar';
 // features
 import { DefaultNavbar } from '../styled-components/navBar-styled-component';
 import EventList from '../features/results/index';
-import CalendarBar from '../features/results/components/CalendarBar.jsx'
+import Calendar from '../features/calendar';
 
 export default function ResultsPage() {
   return (
     <div className='results-page-container'>
-      <CalendarBar />
+      
       <DefaultNavbar>
         <Navbar />
       </DefaultNavbar>
+      <Calendar displayType='calendar-bar'/>
       <EventList />
     </div>
   );
