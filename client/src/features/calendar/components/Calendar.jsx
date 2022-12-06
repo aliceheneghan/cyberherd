21c0050274cf97f10a1a1f8bb6524f4f64432c61
aliceheneghan <heneghan31@gmail.com>
2022-12-06 13:19:05 +0100

    removed dayClassNames param and replace with sevenDayGrid, added if statement to determine if daysOfWeek array and prefixWeekdays need to be populated

--- client/src/features/calendar/components/Calendar.jsx
+++ client/src/features/calendar/components/Calendar.jsx
@@ -19,10 +19,7 @@ import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 // components
 import Cell from './Cell';
 
-// data
-const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-export default function Calendar({ displayType, classNames }) {
+export default function Calendar({ displayType, sevenDayGrid }) {
   // navigate
   const navigate = useNavigate();
 
@@ -37,55 +34,60 @@ export default function Calendar({ displayType, classNames }) {
   const prevMonth = () => sub(currentDate, { months: 1 });
   const nextMonth = () => add(currentDate, { months: 1 });
 
+  // data
+  let daysOfWeek = [];
+  if (sevenDayGrid) {
+    daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  }
+
   // date variables (extracted from currentDate)
   const monthStart = startOfMonth(currentDate);
   const monthEnd = endOfMonth(currentDate);
   const monthDays = eachDayOfInterval({ start: monthStart, end: monthEnd }); // returns an array of objects
-  const prefixWeekdays = monthStart.getDay() - 1; // getDay calculates days of week before the start of the month based on Sunday as the first weekday, so -1 needed to align with Monday week start
+  let prefixWeekdays = 0;
+  if (sevenDayGrid) {
+    prefixWeekdays = monthStart.getDay() - 1; // getDay calculates days of week before the start of the month based on Sunday as the first weekday, so -1 needed to align with Monday week start
+  }
 
   return (
     <div className={displayType}>
       <div className="calendar-month">
         <Cell handler={changeMonth} handlerParam={prevMonth}>
-          <HiChevronLeft className='arrows arrow-left' />
+          <HiChevronLeft className="arrows arrow-left" />
         </Cell>
         {/* formats JS date into month and year */}
         <Cell>{format(currentDate, 'LLLL yy')}</Cell>
         <Cell handler={changeMonth} handlerParam={nextMonth}>
-        <HiChevronRight className='arrows arrow-left' />
+          <HiChevronRight className="arrows arrow-left" />
         </Cell>
       </div>
 
-    <div className={classNames}>
-      {daysOfWeek.map((day) => (
-        <Cell key={day} className="weekdays">
-          {day}
-        </Cell>
-      ))}
+      <div className={`${sevenDayGrid} calendar-days`}>
+        {/* Days of week and prefixWeekdays only included if sevenDayGrid class passed to Calendar component */}
+        {daysOfWeek.map((day) => (
+          <Cell key={day} className="weekdays">
+            {day}
+          </Cell>
+        ))}
 
-          {Array.from({ length: prefixWeekdays }).map((_, index) => (
-            <Cell key={index} className="date-cell"></Cell>
-          ))}
+        {Array.from({ length: prefixWeekdays }).map((_, index) => (
+          <Cell key={index} className="date-cell"></Cell>
+        ))}
 
-          {monthDays.map((date, i) => {
-            const formattedDate = `${format(date, 'yyyy')}-${format(
-              date,
-              'LL'
-            )}-${format(date, 'dd')}`;
+        {monthDays.map((monthDay, i) => {
+          const formattedDate = `${format(monthDay, 'yyyy')}-${format(
+            monthDay,
+            'LL'
+          )}-${format(monthDay, 'dd')}`;
 
-            return (
-              <Cell
-                key={i}
-                handler={navigateToDateResults}
-                handlerParam={formattedDate}
-                className="date-cell"
-              >
-                {/* formats JS date into day */}
-                {format(date, 'd')}
-              </Cell>
-            );
-          })}
-        </div>
-      </div>
-  );
-}
+          return (
+            <Cell
+              key={i}
+              handler={navigateToDateResults}
+              handlerParam={formattedDate}
+              className="date-cell"
+            >
+              {/* formats JS date into day */}
+              {format(monthDay, 'd')}
+            </Cell>
+            
