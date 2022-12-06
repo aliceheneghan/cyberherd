022d6104bf5edad1f9f01b46b1e4eef414cf2f56
aliceheneghan <heneghan31@gmail.com>
2022-12-06 14:36:01 +0100

    added if statement to determine if monthDay needs highlightDateClicked className or not

--- client/src/features/calendar/components/Calendar.jsx
+++ client/src/features/calendar/components/Calendar.jsx
@@ -1,6 +1,6 @@
 // libraries
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import {
   format,
   startOfMonth,
@@ -20,32 +20,32 @@ import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 import Cell from './Cell';
 
 export default function Calendar({ displayType, sevenDayGrid }) {
+  // initial data
+  const { date } = useParams(); // Pulls date selected from calendar passed in url param
+  let daysOfWeek = [];
+  let prefixWeekdays = 0;
+  let highlightDateClicked = '';
+
   // navigate
   const navigate = useNavigate();
 
   // state
-  const [currentDate, setCurrentDate] = useState(new Date());
+  const [dateSelected, setDateSelected] = useState(new Date()); // initial value is today's date
 
   // handlers
-  const changeMonth = (cb) => setCurrentDate(cb);
+  const changeMonth = (cb) => setDateSelected(cb);
   const navigateToDateResults = (date) => navigate(`/results/${date}`);
 
   // callbacks
-  const prevMonth = () => sub(currentDate, { months: 1 });
-  const nextMonth = () => add(currentDate, { months: 1 });
+  const prevMonth = () => sub(dateSelected, { months: 1 });
+  const nextMonth = () => add(dateSelected, { months: 1 });
 
-  // data
-  let daysOfWeek = [];
-  if (sevenDayGrid) {
-    daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-  }
-
-  // date variables (extracted from currentDate)
-  const monthStart = startOfMonth(currentDate);
-  const monthEnd = endOfMonth(currentDate);
+  // date variables (extracted from dateSelected)
+  const monthStart = startOfMonth(dateSelected);
+  const monthEnd = endOfMonth(dateSelected);
   const monthDays = eachDayOfInterval({ start: monthStart, end: monthEnd }); // returns an array of objects
-  let prefixWeekdays = 0;
-  if (sevenDayGrid) {
+  if (displayType === 'calendar-block') {
+    daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     prefixWeekdays = monthStart.getDay() - 1; // getDay calculates days of week before the start of the month based on Sunday as the first weekday, so -1 needed to align with Monday week start
   }
 
@@ -56,7 +56,7 @@ export default function Calendar({ displayType, sevenDayGrid }) {
           <HiChevronLeft className="arrows arrow-left" />
         </Cell>
         {/* formats JS date into month and year */}
-        <Cell>{format(currentDate, 'LLLL yy')}</Cell>
+        <Cell>{format(dateSelected, 'LLLL yy')}</Cell>
         <Cell handler={changeMonth} handlerParam={nextMonth}>
           <HiChevronRight className="arrows arrow-left" />
         </Cell>
@@ -79,13 +79,21 @@ export default function Calendar({ displayType, sevenDayGrid }) {
             monthDay,
             'LL'
           )}-${format(monthDay, 'dd')}`;
+          console.log(formattedDate, date);
+
+          // sets or empties highlightDateClicked className
+          if (formattedDate === date) {
+            highlightDateClicked = 'highlight-date-clicked';
+          } else {
+            highlightDateClicked = '';
+          }
 
           return (
             <Cell
               key={i}
               handler={navigateToDateResults}
               handlerParam={formattedDate}
-              className="date-cell"
+              className={`date-cell ${highlightDateClicked}`}
             >
               {/* formats JS date into day */}
               {format(monthDay, 'd')}
