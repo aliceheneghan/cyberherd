9510a52da7b51abff600f9c157a1ae7efc7cef59
aliceheneghan <heneghan31@gmail.com>
2022-12-07 17:46:52 +0100

    created h1 with band name

--- client/src/features/results/components/EventCard.jsx
+++ client/src/features/results/components/EventCard.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function EventCard({ className, children }) {
   return (
     <div className={className}>
-     <div className='info-wrapper'>
-
-     </div>
+      <div className="info-wrapper">
+        <h1>{children.name.bandName}</h1>
+      </div>
     </div>
   );
 }
