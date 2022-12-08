f7dedefdeb1faf507e62a23040e3b51a7279ef8f
aliceheneghan <heneghan31@gmail.com>
2022-12-08 11:34:28 +0100

    added if statement to determine tileFontSize className according to event name length

--- client/src/features/results/components/EventCard.jsx
+++ client/src/features/results/components/EventCard.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
 export default function EventCard({ className, children }) {
+  const title = children.name.bandName;
+
+  // title length/size logic
+  let titleFontSize = 'title-big';
+  if (title.length > 11) {
+    titleFontSize ='title-small';
+  }
+  
   return (
     <div className={className}>
-      <div className="info-wrapper">
-        <h1>{children.name.bandName}</h1>
+      <div className="event-wrapper">
+        <div className={`title ${titleFontSize}`}>{title}</div>
         <div className="line-wrapper">
-          <div>Location:</div> <div>{children.location}</div>
+          <div className="line-space-between">
+            <div>Location:</div> <div>{children.location}</div>
+          </div>
+          <div className="line-space-between">
+            <div>Genre:</div>
+            <div>{children.genre}</div>
+          </div>
         </div>
-        <div className="line-wrapper">
-          <div>Genre:</div>
-          <div>{children.genre}</div>
-        </div>
-        <div className='more'>...more</div>
+        <div className="more">...more</div>
       </div>
     </div>
   );
