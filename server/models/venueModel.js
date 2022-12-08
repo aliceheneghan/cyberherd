7d729e61d2a75ceedb53db1ea7f324e13c99b480
daanvanderputte <daanvanderputte@protonmail.com>
2022-12-08 16:51:46 +0100

    added longitude and latitude to venueModel

--- server/models/venueModel.js
+++ server/models/venueModel.js
@@ -7,6 +7,8 @@ const venueSchema = new Schema({
   name: { type: String, required: false },
   location: {
     address: { type: String },
+    longitude: { type: Number },
+    latitude: { type: Number },
     neighborhood: {
       type: String,
       enum: [
@@ -34,7 +36,10 @@ const venueSchema = new Schema({
       required: false,
     },
   },
-  venueType: { type: String, enum: ['bar', 'café', 'club', 'concert hall', 'cultural center'] },
+  venueType: {
+    type: String,
+    enum: ['bar', 'café', 'club', 'concert hall', 'cultural center'],
+  },
   venueURL: { type: String },
   photoURL: { type: String },
   event: { type: Schema.Types.ObjectId, ref: 'event' },
