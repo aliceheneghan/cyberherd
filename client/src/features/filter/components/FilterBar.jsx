31bab0bc38da6a85af06fa93ce352fa5b1133d80
daanvanderputte <daanvanderputte@protonmail.com>
2022-12-11 17:02:38 +0100

    added GenreFilter component

--- client/src/features/filter/components/FilterBar.jsx
+++ client/src/features/filter/components/FilterBar.jsx
@@ -4,16 +4,50 @@ import React, { useState } from 'react';
 // style
 import './_filter-bar.scss';
 
+// icons
+import { HiChevronDown } from 'react-icons/hi2';
+
 // components
 // (4 filtes)
+import GenreFilter from './GenreFilter';
 
 export default function FilterBar() {
+  const [showFilter, setShowFilter] = useState(false);
+
+  const handleClick = (event) => {
+    setShowFilter((current) => !current);
+  };
   return (
+    <div className='filter-component'>
     <div className="filter-bar">
-      <div>Filter</div>
-      <div>Filter</div>
-      <div>Filter</div>
-      <div>Filter</div>
+      <div className="filter-category">
+        Genre
+        <div onClick={handleClick}>
+          <HiChevronDown className="arrow-down" />
+        </div>
+      </div>
+      <div className="filter-category">
+        Neighborhood
+        <div>
+          <HiChevronDown className="arrow-down" />
+        </div>
+      </div>
+      <div className="filter-category">
+        Price
+        <div>
+          <HiChevronDown className="arrow-down" />
+        </div>
+      </div>
+      <div className="filter-category">
+        Venue Type
+        <div>
+          <HiChevronDown className="arrow-down" />
+        </div>
+      </div>
     </div>
+    <div className='filter-checkboxes'>
+        {showFilter && <GenreFilter/>}
+    </div>
+    </div>   
   );
 }
