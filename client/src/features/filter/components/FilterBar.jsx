defbd9de1df6a5af3d673bab796faaf313dc94eb
daanvanderputte <daanvanderputte@protonmail.com>
2022-12-11 15:18:12 +0100

    created FilterBar

---
+++ client/src/features/filter/components/FilterBar.jsx
@@ -0,0 +1,19 @@
+// libraries
+import React, { useState } from 'react';
+
+// style
+import './_filter-bar.scss';
+
+// components
+// (4 filtes)
+
+export default function FilterBar() {
+  return (
+    <div className="filter-bar">
+      <div>Filter</div>
+      <div>Filter</div>
+      <div>Filter</div>
+      <div>Filter</div>
+    </div>
+  );
+}
